6fb5724b072b248d674ba6443045cc572d87ec84
RMHIVSM <rmhivsm@ucl.ac.uk>
2023-08-29 13:58:45 +0100

    explore repeats update

--- scripts/quick_checks.py
+++ scripts/quick_checks.py
@@ -1,5 +1,11 @@
 import pandas as pd
-import numpy as np
+#import numpy as np
+
+ds = pd.read_csv("../data/SUBJECT_ID_to_NOTES_1a.csv").sample(frac=0.1)
+print(len(ds))
+
+ds.to_csv(path_or_buf="../data/SUBJECT_ID_to_NOTES_1a_10percent.csv")
+"""
 
 df = pd.read_csv("../data/Deduplication_Example_10.csv")
 df.columns = ["TEXT"]
@@ -10,5 +16,5 @@ df2 = pd.DataFrame(data=d, columns=["TEXT"])
 df3 = pd.concat([df, df2], axis=0, ignore_index=True)
 
 df3.to_csv(path_or_buf="../data/Deduplication_Example_10-2000.csv")
-
+"""
 a=1
